4c70e8cf88f08ebee00cbda0d2003fe9a5764593
Lithl <blanketyblankman@gmail.com>
2019-05-11 06:49:24 -0700

    add title menu to game

--- src/components/flood-app/index.ts
+++ src/components/flood-app/index.ts
@@ -1,11 +1,21 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element';
-import { customElement, observe, property /*, query*/ } from '@polymer/decorators';
+import { customElement, observe, property, query } from '@polymer/decorators';
 
 import '../keyboard-handler';
+import { TitleMenu } from '../title-menu';
 import * as template from './template.html';
+import * as Util from '../../util';
 
 import '../../common.scss?name=common';
 import './index.scss?name=main';
+
+enum State {
+  SIGNIN = 'signin',
+  TITLE_MENU = 'title menu',
+  INTRO_SEQUENCE = 'intro sequence',
+  FIELD = 'field',
+  OPTIONS_MENU = 'options',
+}
 
 /**
  * Main app; much of the state will be stored here, and the only element of the
@@ -15,16 +25,14 @@ import './index.scss?name=main';
  */
 @customElement('flood-app')
 export class FloodApp extends PolymerElement {
+  @property() googleUser!: gapi.auth2.GoogleUser;
+
+  private gameState_ = State.SIGNIN;
+  @query('#titleMenu') protected titleMenu_!: TitleMenu;
+
   static get template() {
     // @ts-ignore
     return html([template]);
-  }
-
-  @property()
-  googleUser!: gapi.auth2.GoogleUser;
-
-  ready() {
-    super.ready();
   }
 
   // reportBug() {
@@ -40,11 +48,30 @@ export class FloodApp extends PolymerElement {
   @observe('googleUser')
   protected googleUserChanged_(user: gapi.auth2.GoogleUser) {
     if (user) {
-      setTimeout(() => {
-        this.style.display = 'block';
-      }, 1500);
+      this.gameState_ = State.TITLE_MENU;
     } else {
-      this.style.display = 'none';
+      this.gameState_ = State.SIGNIN;
+    }
+  }
+
+  @observe('gameState_')
+  protected gameStateChanged_() {
+    switch (this.gameState_) {
+      case State.SIGNIN:
+        this.titleMenu_.style.opacity = '0';
+        break;
+      case State.TITLE_MENU:
+        this.titleMenu_.style.opacity = '1';
+        this.titleMenu_.grabFocus();
+        break;
+      case State.INTRO_SEQUENCE:
+        break;
+      case State.FIELD:
+        break;
+      case State.OPTIONS_MENU:
+        break;
+      default:
+        Util.assertUnreachable(this.gameState_);
     }
   }
 
