0afbbc52100791b605c46e70b1593bfd5a613cb5
Lithl <blanketyblankman@gmail.com>
2019-05-16 08:41:30 -0700

    title-menu code cleanup

--- src/components/title-menu/index.ts
+++ src/components/title-menu/index.ts
@@ -1,6 +1,7 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element';
 import { customElement, query, property } from '@polymer/decorators';
-import /*{ PaperListboxElement } from*/ '@polymer/paper-listbox/paper-listbox';
+import { PaperListboxElement } from '@polymer/paper-listbox/paper-listbox';
+import '@polymer/paper-listbox/paper-listbox';
 import '@polymer/paper-item/paper-item';
 
 import * as template from './template.html';
@@ -10,7 +11,7 @@ import './index.scss?name=title';
 
 @customElement('title-menu')
 export class TitleMenu extends PolymerElement {
-  @query('#menuOptions') private menuOptions_!: HTMLElement;
+  @query('#menuOptions') private menuOptions_!: PaperListboxElement;
   @property() protected saveGameAvailable_ = false;
 
   static get template() {
@@ -22,17 +23,11 @@ export class TitleMenu extends PolymerElement {
     this.menuOptions_.focus();
   }
 
-  protected itemSelected_(e: CustomEvent) {
-    switch (e.detail.selected) {
-      case 'newGame':
-        // move game state to INTRO_SEQUENCE
-        break;
-      case 'loadGame':
-        // move game state to FIELD, loading saved game data
-        break;
-      case 'options':
-        // move game state to OPTIONS_MENU
-        break;
-    }
+  show() {
+    this.style.opacity = '1';
+  }
+
+  hide() {
+    this.style.opacity = '0';
   }
 }
