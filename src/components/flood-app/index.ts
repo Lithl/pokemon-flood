aef7cf5255bd9a3de5f17d7050ac47be1d902b90
Lithl <blanketyblankman@gmail.com>
2019-05-16 08:41:48 -0700

    add options menu flow from title screen

--- src/components/flood-app/index.ts
+++ src/components/flood-app/index.ts
@@ -1,15 +1,18 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element';
-import { customElement, observe, property, query } from '@polymer/decorators';
+import { customElement, listen, observe, property, query } from '@polymer/decorators';
+import { DeclarativeEventListeners } from '@polymer/decorators/lib/declarative-event-listeners';
 
 import '../keyboard-handler';
 import { TitleMenu } from '../title-menu';
+import { OptionsMenu } from '../options-menu';
+import { Action } from '../keyboard-handler';
 import * as template from './template.html';
 import * as Util from '../../util';
 
 import '../../common.scss?name=common';
 import './index.scss?name=main';
 
-enum State {
+export enum State {
   SIGNIN = 'signin',
   TITLE_MENU = 'title menu',
   INTRO_SEQUENCE = 'intro sequence',
@@ -24,15 +27,25 @@ enum State {
  * being in a shadow root well)
  */
 @customElement('flood-app')
-export class FloodApp extends PolymerElement {
+export class FloodApp extends DeclarativeEventListeners(PolymerElement) {
   @property() googleUser!: gapi.auth2.GoogleUser;
 
-  private gameState_ = State.SIGNIN;
+  @property({
+    type: String,
+    observer: 'gameStateChanged_',
+  }) private gameState_ = State.SIGNIN;
+
   @query('#titleMenu') protected titleMenu_!: TitleMenu;
+  @query('#optionsMenu') protected optionsMenu_!: OptionsMenu;
 
   static get template() {
     // @ts-ignore
     return html([template]);
+  }
+
+  @listen('set-game-state', document)
+  setGameState_(e: Event) {
+    this.gameState_ = (e as CustomEvent).detail.newState as State;
   }
 
   // reportBug() {
@@ -54,14 +67,15 @@ export class FloodApp extends PolymerElement {
     }
   }
 
-  @observe('gameState_')
-  protected gameStateChanged_() {
-    switch (this.gameState_) {
+  protected gameStateChanged_(curr: State, prev: State) {
+    switch (curr) {
       case State.SIGNIN:
-        this.titleMenu_.style.opacity = '0';
+        this.titleMenu_.hide();
+        this.optionsMenu_.hide();
         break;
       case State.TITLE_MENU:
-        this.titleMenu_.style.opacity = '1';
+        this.optionsMenu_.hide();
+        this.titleMenu_.show();
         this.titleMenu_.grabFocus();
         break;
       case State.INTRO_SEQUENCE:
@@ -69,13 +83,36 @@ export class FloodApp extends PolymerElement {
       case State.FIELD:
         break;
       case State.OPTIONS_MENU:
+        this.titleMenu_.hide();
+        this.optionsMenu_.show();
+        this.optionsMenu_.returnState = prev;
+        this.optionsMenu_.grabFocus();
         break;
       default:
-        Util.assertUnreachable(this.gameState_);
+        Util.assertUnreachable(curr);
     }
   }
 
   protected handleKeyPressed_(e: CustomEvent) {
+    if (!e.detail.down && this.gameState_ === State.OPTIONS_MENU) {
+      if (e.detail.action === Action.LEFT || e.detail.action === Action.RIGHT) {
+        this.optionsMenu_.handleKeyPressed(e.detail.action);
+      }
+    }
     console.log(e);
+  }
+
+  protected menuItemSelected_(e: CustomEvent) {
+    switch (e.detail.selected) {
+      case 'newGame':
+        // move game state to INTRO_SEQUENCE
+        break;
+      case 'loadGame':
+        // move game state to FIELD, loading saved game data
+        break;
+      case 'options':
+        this.gameState_ = State.OPTIONS_MENU;
+        break;
+    }
   }
 }
