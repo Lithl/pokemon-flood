e062d7d23803d93d69230d9757b859cb1a0bedd0
Lithl <blanketyblankman@gmail.com>
2019-06-18 11:23:43 -0700

    Completed keybindings menu, except for storing values in database

--- src/components/options-menu/index.ts
+++ src/components/options-menu/index.ts
@@ -4,20 +4,48 @@ import { PaperListboxElement } from '@polymer/paper-listbox/paper-listbox';
 import { PaperItemElement } from '@polymer/paper-item/paper-item';
 
 import { State } from '../flood-app';
-import { Action } from '../keyboard-handler';
+import { Action, Service } from '../../options';
 import * as template from './template.html';
+import { KeybindingsMenu } from './keybindings';
 
 import '../../common.scss?name=common';
 import './index.scss?name=options';
+
+const options = Service.getGameOptions();
+
+interface OptionsModel {
+  name: string;
+  label: string;
+  possibleValues: string[];
+  current: string;
+}
 
 @customElement('options-menu')
 export class OptionsMenu extends PolymerElement {
   @property() returnState: State = State.TITLE_MENU;
   @query('#menuOptions') private menuOptions_!: PaperListboxElement;
+  @query('#keybindings') private keybindings_!: KeybindingsMenu;
+  @property() private options_: OptionsModel[] = [];
 
   static get template() {
     // @ts-ignore
     return html([template]);
+  }
+
+  ready() {
+    super.ready();
+
+    options.getOptionNames().forEach((name) => {
+      const data = options.getOptionData(name);
+      if (!data) return;
+
+      this.options_.push({
+        name,
+        label: data.label,
+        possibleValues: data.possible as string[],
+        current: data.current as string,
+      });
+    });
   }
 
   grabFocus() {
@@ -42,6 +70,7 @@ export class OptionsMenu extends PolymerElement {
         options.selectNext();
       }
     }
+    // this.keybindings_.handleKeyPressed(action);
   }
 
   protected optionItemSelected_(e: CustomEvent) {
@@ -62,6 +91,8 @@ export class OptionsMenu extends PolymerElement {
         break;
       case 'keybindings':
         // move to keybindings options menu
+        this.keybindings_.show();
+        this.keybindings_.grabFocus();
         break;
       case 'confirm':
         this.dispatchEvent(new CustomEvent('set-game-state', {
