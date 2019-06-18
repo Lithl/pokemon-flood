d7611a6c3c4cf40601503b380fc687fad4dec1a4
Lithl <blanketyblankman@gmail.com>
2019-06-18 11:22:41 -0700

    Add optional type

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
 export function assertUnreachable(_: never): never {
   throw new Error('This function should not be called');
 }
+
+export type Optional<T> = T | undefined;
+
+export type Nullable<T> = T | null;
