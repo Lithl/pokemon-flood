0ffdefdafbbf57f1dab20b038a93aea40382aa15
Lithl <blanketyblankman@gmail.com>
2019-06-19 04:24:37 -0700

    support get/post keybindings

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const dbConfig = require('./db.config.json');
+const pool = mysql.createPool({
+  connectionLimite: 10,
+  host: 'localhost',
+  database: 'flood',
+  user: dbConfig.user,
+  password: dbConfig.password,
+});
 
 const app = express();
 
@@ -20,5 +31,42 @@ for (const key in whitelist) {
   app.get(`/${key}`, (req, res) => res.sendFile(`${__dirname}/${file}`));
 }
 
-app.listen(3001, () => console.log('Server running on port 3001'));
+app.use(bodyParser.json());
+app.post('/user/:id/keys', (req, res) => {
+  const valuesPlaceholder = Object.keys(req.body).map((action) =>
+      '(?, ?, ?, ?)').join(',');
+  const values = Object.keys(req.body).reduce((arr, action) => {
+    arr.push(
+      req.params.id,
+      action,
+      req.body[action][0].trim() || null,
+      req.body[action][1].trim() || null);
+    return arr;
+  }, []);
+  pool.query(`replace into keybindings (id, action, binding1, binding2)
+      values ${valuesPlaceholder}`, values, (err) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+    res.sendStatus(200);
+  });
+});
+app.get('/user/:id/keys', (req, res) => {
+  pool.query('select action, binding1, binding2 from keybindings where id = ?',
+      req.params.id, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+    const body = results.reduce((obj, row) => {
+      obj[row.action] = [row.binding1 || '', row.binding2 || ''];
+      return obj;
+    }, {});
+    res.status(200).send(body);
+  });
+});
 
+app.listen(3001, () => console.log('Server running on port 3001'));
