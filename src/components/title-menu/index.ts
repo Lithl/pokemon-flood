e24a2d30e56e324dd85c38062b953fa6711e7509
Lithl <blanketyblankman@gmail.com>
2019-06-27 01:50:25 -0700

    enabled selecting options from title menu

--- src/components/title-menu/index.ts
+++ src/components/title-menu/index.ts
@@ -3,13 +3,22 @@ import { customElement, property, query } from '@polymer/decorators';
 import { FloodScreen } from '../flood-screens';
 import { PaperListboxElement } from '@polymer/paper-listbox/paper-listbox';
 import '@polymer/paper-item/paper-item';
-import { PaperItemElement } from '@polymer/paper-item/paper-item';
 
 import * as template from './template.html';
 import { MenuBehavior } from '../../mixins/menu-behavior';
-import { Constructor, Optional } from '../../util';
+import { Constructor, assertUnreachable } from '../../util';
+import { Service } from '../../options';
+import { State } from '../flood-app';
 
 import './index.scss?name=title-menu';
+
+const properties = Service.getGameProperties();
+
+enum TitleMenuOptions {
+  NEW_GAME = 0,
+  CONTINUE = 1,
+  OPTIONS = 2,
+}
 
 @customElement('title-menu')
 export class TitleMenu extends MenuBehavior
@@ -33,38 +42,43 @@ export class TitleMenu extends MenuBehavior
   ready() {
     super.ready();
     setTimeout(() => {
-      const menuItems: Optional<PaperItemElement>[] =
+      this.menuItems_ =
           [...this.menuOptions_.querySelectorAll('paper-item')];
       if (!this.saveGameAvailable_) {
-        menuItems.splice(1, 0, undefined);
+        this.menuItems_.splice(1, 0, undefined);
+        // TODO: recompute menuItem_ when saveGameAvailable_ changes
       }
-      menuItems[this.current_]!.classList.toggle('selected', true);
+      this.menuItems_[this.current_]!.classList.toggle('selected', true);
     });
   }
 
-  show() {
-    super.show();
-    this.isShowing_ = true;
-  }
-
-  hide() {
-    super.hide();
-    this.isShowing_ = false;
-  }
-
   protected currentChanged_(current: number, previous: number) {
-    const menuItems: Optional<PaperItemElement>[] =
-        [...this.menuOptions_.querySelectorAll('paper-item')];
     if (!this.saveGameAvailable_) {
       if (current === 1) {
         if (previous === 2) this.current_ = 0;
         else this.current_ = 2;
         current = this.current_;
       }
-      menuItems.splice(1, 0, undefined);
     }
 
-    menuItems[previous]!.classList.toggle('selected', false);
-    menuItems[current]!.classList.toggle('selected', true);
+    super.currentChanged_(current, previous);
+  }
+
+  protected confirmAt_(current: number) {
+    const opt = current as TitleMenuOptions;
+    console.log('confirming in title screen');
+    switch (opt) {
+      case TitleMenuOptions.NEW_GAME:
+        console.log('new game');
+        break;
+      case TitleMenuOptions.CONTINUE:
+        console.log('continue save');
+        break;
+      case TitleMenuOptions.OPTIONS:
+        properties.setProperty('game-state', State.OPTIONS_MENU);
+        break;
+      default:
+        assertUnreachable(opt);
+    }
   }
 }
