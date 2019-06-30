b2cb3adc0ce327684608fa8df42655f68afd2974
Lithl <blanketyblankman@gmail.com>
2019-06-29 23:49:38 -0700

    add new utility functions

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,25 @@ export function assertUnreachable(_: never): never {
   throw new Error('This function should not be called');
 }
 
+export function lerpPct(a: number, b: number, p: number) {
+  if (p < 0 || p > 1) {
+    throw new TypeError('p must be in the range [0,1]');
+  }
+  return a + p * (b - a);
+}
+
+export function lerpVal(a: number, b: number, p: number) {
+  if (p < Math.min(a, b) || p > Math.max(a, b)) {
+    throw new TypeError('p must be between a and b');
+  }
+  p = (p - a) / (b - a);
+  return a + p * (b - a);
+}
+
+export function clamp(val: number, min: number, max: number) {
+  return Math.min(Math.max(val, min), max);
+}
+
 export type Optional<T> = T | undefined;
 
 export type Nullable<T> = T | null;
