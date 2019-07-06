8071f3aad631349ef6c5fbc0a233b80e6a8c06d2
Lithl <blanketyblankman@gmail.com>
2019-07-05 23:41:57 -0700

    add event handling for advancing dialogue

--- src/components/dialogue-box/index.ts
+++ src/components/dialogue-box/index.ts
@@ -8,11 +8,27 @@ import { Service } from '../../options';
 import './index.scss?name=dialogue-box';
 
 const options = Service.getGameOptions();
+const keybindings = Service.getKeybindOptions();
+
+interface DialgoueResponse {
+  text: string;
+  onSelect: () => void;
+}
+
+interface PlayDialogueOptions {
+  onConfirm?: (avatar?: Sprite) => void;
+  onComplete?: (avatar?: Sprite) => void;
+  responses?: DialgoueResponse[];
+}
+
+interface LineData extends PlayDialogueOptions {
+  text: string;
+}
 
 @customElement('dialogue-box')
 export class DialogueBox extends PolymerElement {
   @property() text = '';
-  @property() lines: string[] = [];
+  @property() lines: Array<LineData | string> = [];
   @query('#box') private box_!: HTMLDivElement;
   @query('#avatar') private avatar_!: HTMLDivElement;
   private sprite_?: Sprite;
@@ -23,11 +39,32 @@ export class DialogueBox extends PolymerElement {
   }
 
   play(line: number) {
-    const currentLine = this.lines[line];
-    if (!currentLine) return;
+    const currentLineData = this.lines[line];
+    if (!currentLineData) return;
+    const currentLine = (currentLineData as LineData).text
+        || currentLineData as string;
+    const playOptions: PlayDialogueOptions = {
+      onConfirm: (currentLineData as PlayDialogueOptions).onConfirm || (() => {
+        this.play(line + 1);
+      }),
+      onComplete: (currentLineData as PlayDialogueOptions).onComplete,
+      responses: (currentLineData as PlayDialogueOptions).responses || [],
+    };
+
+    const keyPressHandler = (kbd: KeyboardEvent) => {
+      const confirm = keybindings.getOption('CONFIRM') as string[];
+      if (confirm.indexOf(kbd.code) >= 0) {
+        this.box_.classList.toggle('next', false);
+        playOptions.onConfirm!(this.sprite_);
+        document.body.removeEventListener('keyup', keyPressHandler);
+      }
+    };
+
     this.text = '';
     const isShowing = this.box_.classList.contains('full');
     this.box_.classList.toggle('full', true);
+    // wait for the `full` transition to complete if the dialogue box wasn't
+    // already showing
     setTimeout(() => {
       if (this.sprite_) this.sprite_.play();
       const currentLineArr = [...currentLine];
@@ -36,11 +73,23 @@ export class DialogueBox extends PolymerElement {
       const speed = (2 - speedIdx) ? speedIdx : .5;
       const interval = setInterval(() => {
         if (currentLineArr.length) {
+          // add next character to dialogue box
           this.text += currentLineArr.shift();
         } else {
+          // all characters have been added
           if (this.sprite_) this.sprite_.stop(true);
           clearInterval(interval);
-          this.box_.classList.toggle('next', true);
+
+          if (playOptions.onComplete) {
+            playOptions.onComplete(this.sprite_);
+          } else {
+            if (playOptions.responses!.length) {
+              // TODO: show response list
+            } else {
+              this.box_.classList.toggle('next', true);
+            }
+            document.body.addEventListener('keyup', keyPressHandler);
+          }
         }
       }, 100 * speed);
     }, isShowing ? 0 : 1000);
